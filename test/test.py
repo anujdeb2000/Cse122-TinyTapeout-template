6513080cab0923ff2d0860b452236eedf61d81da
Anuj Deb <anujdeb2000@gmail.com>
2026-03-03 21:19:02 -0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,51 +5,51 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
+
     # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
-    # Reset
-    dut._log.info("Reset")
+
+    #Reset 
+    dut._log.info("Resetting the module")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
+    dut.rst_n.value = 0  # Reset active low
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    dut.rst_n.value = 1  # Reset released
+
+    #INIT CHECK
     dut._log.info("Check for INIT state")
-    assert dut.uo_out.value == 0
-    dut._log.info("start signal")
-    dut.ui_in.value = 1 #start == 1
+    # uo_out[0] = wingame, uo_out[1] = losegame
+    assert int(dut.uo_out.value) == 0
+
+    #start signal
+    dut._log.info("Triggering start signal")
+    dut.ui_in.value = 1 # ui_in[0] is start
     await ClockCycles(dut.clk, 2)
     dut.ui_in.value = 0
-    # Set the input values you want to test
-    #dut.ui_in.value = 20
-    #dut.uio_in.value = 30
-    await ClockCycles(dut.clk, 10)    #wait till sequence is loaded  (5 cycles)
+    
+    # Wait till sequence is loaded
+    await ClockCycles(dut.clk, 10)
+
+    # --- Verify Game Progress ---
     dut._log.info("Making sure game is still ongoing")
     assert int(dut.uo_out.value) == 0
+
+    #Input signal
     dut._log.info("Input button 1")
-    dut.ui_in.value = 4 # 0100 binary (ui[2]) 
+    dut.ui_in.value = 4 # 0100 binary (ui_in[2])
     await ClockCycles(dut.clk, 5)
     dut.ui_in.value = 0
+    
+    # Wait for logic to process
     await ClockCycles(dut.clk, 5)
+    
     current_status = int(dut.uo_out.value)
     dut._log.info(f"Final output value: {current_status}")
-    #checking output values (wingame, losegame)
-    dut._log.info("Simulation Finished successfully")
-
-
     
-    # Wait for one clock cycle to see the output values
-    #await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    #assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut._log.info("Simulation Finished successfully")
